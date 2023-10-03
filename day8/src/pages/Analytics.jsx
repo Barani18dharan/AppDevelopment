94eed300dfa0838db53d9ee5c5d511b659343cb4
Barani18dharan <126306837+Barani18dharan@users.noreply.github.com>
2023-10-03 10:02:08 +0530

    Add files via upload

---
+++ day8/src/pages/Analytics.jsx
@@ -0,0 +1,15 @@
+const Analytics = () => {
+  return <div className="title"><u><b>Today's Meetings</b></u><br/>
+  <h2>
+    09.00 a.m - 11.00 a.m  : one to one meeting with Adam
+    <br />
+    <br />
+    02.00 p.m - 03.00 p.m : conference meeting with Tex and co
+    <br />
+    <br />
+    06.00 p.m - 07.00 p.m : team meeting
+  </h2>
+  </div>;
+};
+
+export default Analytics;
---
+++ day8/src/pages/Messages.jsx
@@ -0,0 +1,5 @@
+const Messages = () => {
+  return <div className="title"> <h2>You don't have any new messages</h2></div>;
+};
+
+export default Messages;
